8b593bfa9381933d9e9b1cd5d66dd757a5336eda
Moewe342 <fynn.choinowski@gmail.com>
2023-10-16 16:31:35 +0200

    isnecromancer

--- src/main/kotlin/de/fuballer/mcendgame/component/dungeon/enemy/custom_entity/necromancer/NecromancerService.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/dungeon/enemy/custom_entity/necromancer/NecromancerService.kt
@@ -4,7 +4,6 @@ import de.fuballer.mcendgame.component.dungeon.enemy.custom_entity.CustomEntityT
 import de.fuballer.mcendgame.component.dungeon.enemy.custom_entity.MinionRepository
 import de.fuballer.mcendgame.component.dungeon.enemy.custom_entity.MinionsEntity
 import de.fuballer.mcendgame.component.dungeon.enemy.custom_entity.summoner.SummonerService
-import org.bukkit.entity.EntityType
 import org.bukkit.entity.Spellcaster
 import org.bukkit.event.entity.EntitySpellCastEvent
 import kotlin.math.min
@@ -13,9 +12,12 @@ class NecromancerService(
     private val minionRepo: MinionRepository,
     private val summonerService: SummonerService,
 ) {
+    private fun isNecromancer(event: EntitySpellCastEvent) =
+        event.entityType == CustomEntityType.NECROMANCER.type
+                && event.entity.customName == CustomEntityType.NECROMANCER.customName
+
     fun onEntitySpellCast(event: EntitySpellCastEvent) {
-        if (event.entityType != EntityType.EVOKER) return
-        if (event.entity.customName != CustomEntityType.NECROMANCER.customName) return
+        if (!isNecromancer(event)) return
 
         if (event.spell == Spellcaster.Spell.SUMMON_VEX)
             onSummonVexSpell(event)
