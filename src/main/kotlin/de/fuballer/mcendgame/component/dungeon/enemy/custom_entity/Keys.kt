7ba41a7f00240b527a6ea2700a3f4432ea73692e
maucon <maucon13@gmail.com>
2023-10-16 17:56:52 +0200

    renaming

--- src/main/kotlin/de/fuballer/mcendgame/component/dungeon/enemy/custom_entity/Keys.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/dungeon/enemy/custom_entity/Keys.kt
@@ -4,8 +4,8 @@ import de.fuballer.mcendgame.MCEndgame
 import org.bukkit.NamespacedKey
 
 object Keys {
-    val DROP_BASE_LOOT = NamespacedKey(MCEndgame.PLUGIN, "drop-base-loot")
-    val DROP_EQUIPMENT = NamespacedKey(MCEndgame.PLUGIN, "drop-equipment")
-    val MAP_TIER = NamespacedKey(MCEndgame.PLUGIN, "map-tier")
-    val IS_MINION = NamespacedKey(MCEndgame.PLUGIN, "is-minion")
+    val DROP_BASE_LOOT = NamespacedKey(MCEndgame.INSTANCE, "drop-base-loot")
+    val DROP_EQUIPMENT = NamespacedKey(MCEndgame.INSTANCE, "drop-equipment")
+    val MAP_TIER = NamespacedKey(MCEndgame.INSTANCE, "map-tier")
+    val IS_MINION = NamespacedKey(MCEndgame.INSTANCE, "is-minion")
 }
