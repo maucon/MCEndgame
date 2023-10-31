4d1dce05e539d65b2bbd4acd80435aa067222f4b
Moewe342 <fynn.choinowski@gmail.com>
2023-10-31 17:18:49 +0100

    functionality for custom hurt/death sounds

---
+++ src/main/kotlin/de/fuballer/mcendgame/component/dungeon/enemy/custom_entity/sound/CustomEntitySoundData.kt
@@ -0,0 +1,7 @@
+package de.fuballer.mcendgame.component.dungeon.enemy.custom_entity.sound
+
+data class CustomEntitySoundData(
+    val hurt: String,
+    val death: String,
+    val ambient: String
+)
---
+++ src/main/kotlin/de/fuballer/mcendgame/component/dungeon/enemy/custom_entity/sound/CustomEntitySounds.kt
@@ -0,0 +1,11 @@
+package de.fuballer.mcendgame.component.dungeon.enemy.custom_entity.sound
+
+import de.fuballer.mcendgame.component.dungeon.enemy.custom_entity.data.CustomEntityType
+
+object CustomEntitySounds {
+    private val SOUNDS = mapOf(
+        Pair(CustomEntityType.NAGA.toString(), CustomEntitySoundData("mcendgame_naga_hurt", "mcendgame_naga_hurt", "mcendgame_naga_hurt"))
+    )
+
+    fun getSounds(type: String) = SOUNDS[type]
+}
