1f601fdfefbd57739e1ab070732b42ace1963ce0
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2023-11-08 20:47:32 +0100

    25 more custom mobs (#29)
    * new mobs
    
    * enabled all mobs again
    
    * making custom mobs silent
    
    * functionality for custom hurt/death sounds
    
    * hurt -> death
    
    * ye
    
    * tp update
    
    * parrot
    
    * I'm blue
    Da ba dee da ba di
    
    * Update src/main/kotlin/de/fuballer/mcendgame/component/dungeon/enemy/custom_entity/sound/CustomEntitySoundService.kt
    
    ---------
    
    Co-authored-by: Moewe342 <fynn.choinowski@gmail.com>
    Co-authored-by: Maurice o'Con <55910883+maucon@users.noreply.github.com>

---
+++ src/main/kotlin/de/fuballer/mcendgame/component/dungeon/enemy/custom_entity/sound/CustomEntitySoundService.kt
@@ -0,0 +1,35 @@
+package de.fuballer.mcendgame.component.dungeon.enemy.custom_entity.sound
+
+import de.fuballer.mcendgame.component.dungeon.enemy.custom_entity.data.DataTypeKeys
+import de.fuballer.mcendgame.framework.annotation.Component
+import de.fuballer.mcendgame.util.PersistentDataUtil
+import org.bukkit.SoundCategory
+import org.bukkit.entity.Entity
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.entity.EntityDeathEvent
+
+@Component
+class CustomEntitySoundService : Listener {
+    @EventHandler
+    fun onEntityDamage(event: EntityDamageEvent) {
+        val entity = event.entity
+        val sounds = getSounds(entity) ?: return
+
+        entity.world.playSound(entity.location, sounds.hurt, SoundCategory.HOSTILE, 1.0f, 1.0f)
+    }
+
+    @EventHandler
+    fun onEntityDeath(event: EntityDeathEvent) {
+        val entity = event.entity
+        val sounds = getSounds(entity) ?: return
+
+        entity.world.playSound(entity.location, sounds.death, SoundCategory.HOSTILE, 1.0f, 1.0f)
+    }
+
+    private fun getSounds(entity: Entity): CustomEntitySoundData? {
+        val type = PersistentDataUtil.getValue(entity, DataTypeKeys.ENTITY_TYPE) ?: return null
+        return CustomEntitySounds.getSounds(type)
+    }
+}
