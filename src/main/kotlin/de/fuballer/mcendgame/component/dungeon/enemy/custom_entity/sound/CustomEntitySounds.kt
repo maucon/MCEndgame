9a13fbf72162044ea4fc3c20b1d308156cf564bd
Moewe342 <fynn.choinowski@gmail.com>
2023-11-08 20:25:37 +0100

    ye

--- src/main/kotlin/de/fuballer/mcendgame/component/dungeon/enemy/custom_entity/sound/CustomEntitySounds.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/dungeon/enemy/custom_entity/sound/CustomEntitySounds.kt
@@ -4,7 +4,8 @@ import de.fuballer.mcendgame.component.dungeon.enemy.custom_entity.data.CustomEn
 
 object CustomEntitySounds {
     private val SOUNDS = mapOf(
-        Pair(CustomEntityType.NAGA.toString(), CustomEntitySoundData("mcendgame_naga_hurt", "mcendgame_naga_death", "mcendgame_naga_hurt"))
+        Pair(CustomEntityType.NAGA.toString(), CustomEntitySoundData("mcendgame_naga_hurt", "mcendgame_naga_death", "mcendgame_naga_hurt")),
+        Pair(CustomEntityType.HARPY.toString(), CustomEntitySoundData("mcendgame_harpy_hurt", "mcendgame_harpy_death", "mcendgame_harpy_hurt"))
     )
 
     fun getSounds(type: String) = SOUNDS[type]
