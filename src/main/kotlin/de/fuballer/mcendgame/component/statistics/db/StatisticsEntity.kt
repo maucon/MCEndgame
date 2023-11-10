27eaa54fa4e3289d4bce3d311c83f2df3ecc03f9
maucon <maucon13@gmail.com>
2023-11-10 17:17:05 +0100

    update statistics

--- src/main/kotlin/de/fuballer/mcendgame/component/statistics/db/StatisticsEntity.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/statistics/db/StatisticsEntity.kt
@@ -1,10 +1,8 @@
 package de.fuballer.mcendgame.component.statistics.db
 
-import de.fuballer.mcendgame.component.custom_entity.data.CustomEntityType
 import de.fuballer.mcendgame.component.statistics.StatisticsSettings
 import de.fuballer.mcendgame.framework.stereotype.Entity
 import org.bukkit.Material
-import org.bukkit.entity.EntityType
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.BookMeta
 import java.util.*
@@ -15,29 +13,16 @@ data class StatisticsEntity(
     var dungeonsCompleted: Int = 0,
     var highestCompletedDungeon: Int = 0,
     var dungeonsOpened: Int = 0,
-    var deaths: Int = 0,
-
     var totalKills: Int = 0,
-    var mobTypeKills: MutableMap<EntityType, Int> =
-        CustomEntityType.entries
-            .associate { it.type to 0 }
-            .toMutableMap(),
+    var deaths: Int = 0,
+    var highestKillstreak: Int = 0,
 
     var damageDealt: Double = 0.0,
     var rawDamageDealt: Double = 0.0,
     var damageTaken: Double = 0.0,
     var rawDamageTaken: Double = 0.0,
 
-    var highestKillstreak: Int = 0,
-    var killstreakThresholdsReached: MutableMap<Int, Int> = mutableMapOf(
-        25 to 0,
-        50 to 0,
-        75 to 0,
-        100 to 0,
-        200 to 0,
-        300 to 0,
-    )
-) : Entity<UUID> {
+    ) : Entity<UUID> {
     fun createBook(): ItemStack {
         val book = ItemStack(Material.WRITTEN_BOOK)
         val bookMeta = book.itemMeta as BookMeta
@@ -57,51 +42,46 @@ data class StatisticsEntity(
     private fun getPages(): List<String> {
         val pages: MutableList<String> = mutableListOf()
 
-        var page1 = "${StatisticsSettings.STATISTICS_BOOK_BASICS_HEADLINE}\n\n"
-        page1 = page1.plus("${StatisticsSettings.DUNGEONS_COMPLETED_TEXT}\n   $dungeonsCompleted\n")
-        page1 = page1.plus("${StatisticsSettings.HIGHEST_DUNGEONS_COMPLETED_TEXT}\n   $highestCompletedDungeon\n")
-        page1 = page1.plus("${StatisticsSettings.DUNGEONS_OPENED_TEXT}\n   $dungeonsOpened\n")
-        page1 = page1.plus("${StatisticsSettings.TOTAL_KILLS_TEXT}\n   $totalKills\n")
-        page1 = page1.plus("${StatisticsSettings.DEATHS_TEXT}\n   $deaths\n")
-        page1 = page1.plus("${StatisticsSettings.KILLSTREAK_TEXT}\n   $highestKillstreak\n")
+        val page1 = createPage(
+            listOf(
+                StatisticsSettings.STATISTICS_BOOK_BASICS_HEADLINE,
+                "",
+                StatisticsSettings.DUNGEONS_OPENED_TEXT,
+                "   $dungeonsOpened",
+                StatisticsSettings.DUNGEONS_COMPLETED_TEXT,
+                "   $dungeonsCompleted",
+                StatisticsSettings.HIGHEST_DUNGEONS_COMPLETED_TEXT,
+                "   $highestCompletedDungeon",
+                StatisticsSettings.TOTAL_KILLS_TEXT,
+                "   $totalKills",
+                StatisticsSettings.DEATHS_TEXT,
+                "   $deaths",
+                StatisticsSettings.KILLSTREAK_TEXT,
+                "   $highestKillstreak"
+            )
+        )
         pages.add(page1)
 
-        var page3 = ""
-        for (mobType in mobTypeKills.keys) {
-            val typeString = "${StatisticsSettings.STATISTICS_BOOK_MOB_TYPE_COLOR}${getMobTypeDisplayName(mobType)}:"
-            val amountString = "${StatisticsSettings.STATISTICS_BOOK_TEXT_COLOR}${mobTypeKills[mobType]}"
-            page3 = page3.plus("$typeString\n   $amountString\n")
-        }
-        pages.add(page3)
-
-        var page4 = "${StatisticsSettings.STATISTICS_BOOK_DAMAGE_HEADLINE}\n\n"
-        page4 = page4.plus("${StatisticsSettings.DAMAGE_DEALT_TEXT}\n   ${String.format("%.2f", damageDealt)}\n")
-        page4 = page4.plus("${StatisticsSettings.RAW_DAMAGE_DEALT_TEXT}\n   ${String.format("%.2f", rawDamageDealt)}\n")
-        page4 = page4.plus("${StatisticsSettings.DAMAGE_TAKEN_TEXT}\n   ${String.format("%.2f", damageTaken)}\n")
-        page4 = page4.plus("${StatisticsSettings.RAW_DAMAGE_TAKEN_TEXT}\n   ${String.format("%.2f", rawDamageTaken)}\n")
-        pages.add(page4)
-
-        var page5 = "${StatisticsSettings.STATISTICS_BOOK_KILLSTREAK_HEADLINE}\n\n"
-        for (killstreakThreshold in killstreakThresholdsReached.keys) {
-            val thresholdString = "${StatisticsSettings.STATISTICS_BOOK_KILLSTREAK_THRESHOLD_COLOR}$killstreakThreshold:"
-            val amountString = "${StatisticsSettings.STATISTICS_BOOK_TEXT_COLOR}${killstreakThresholdsReached[killstreakThreshold]}"
-            page5 = page5.plus("$thresholdString\n   $amountString\n")
-        }
-        pages.add(page5)
+        val page2 = createPage(
+            listOf(
+                StatisticsSettings.STATISTICS_BOOK_DAMAGE_HEADLINE,
+                "",
+                "",
+                StatisticsSettings.DAMAGE_DEALT_TEXT,
+                "   ${String.format("%.2f", damageDealt)}",
+                StatisticsSettings.RAW_DAMAGE_DEALT_TEXT,
+                "   ${String.format("%.2f", rawDamageDealt)}",
+                "",
+                StatisticsSettings.DAMAGE_TAKEN_TEXT,
+                "   ${String.format("%.2f", damageTaken)}",
+                StatisticsSettings.RAW_DAMAGE_TAKEN_TEXT,
+                "   ${String.format("%.2f", rawDamageTaken)}"
+            )
+        )
+        pages.add(page2)
 
         return pages
     }
 
-    private fun getMobTypeDisplayName(mobType: EntityType): String {
-        var displayName = ""
-
-        val words = mobType.name.split("_")
-        for (word in words) {
-            val lowerCaseWord = word.lowercase()
-            val uppercaseWord = lowerCaseWord[0].uppercase() + lowerCaseWord.substring(1)
-            displayName = displayName.plus("$uppercaseWord ")
-        }
-
-        return displayName.trim()
-    }
+    private fun createPage(lines: List<String>) = lines.joinToString("\n")
 }
