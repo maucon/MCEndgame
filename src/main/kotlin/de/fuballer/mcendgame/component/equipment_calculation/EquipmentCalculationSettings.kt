faaf87b34c800a9b56c81576daded1ee03e67681
Maurice o'Con <55910883+maucon@users.noreply.github.com>
2024-01-17 23:13:47 +0100

    update plugin.yml (#82)
    * update plugin.yml
    
    * remove equipment calculation command

--- src/main/kotlin/de/fuballer/mcendgame/component/equipment_calculation/EquipmentCalculationSettings.kt
+++
@@ -1,7 +0,0 @@
-package de.fuballer.mcendgame.component.equipment_calculation
-
-object EquipmentCalculationSettings {
-    const val COMMAND_NAME = "dungeon-equipment-calculation"
-
-    const val NUMBER_DECIMAL_PLACES = 2
-}
