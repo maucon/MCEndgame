3667b079519f03e12c82fd02c1a735a6c887a9cd
Moewe342 <fynn.choinowski@gmail.com>
2024-01-17 22:30:22 +0100

    stop entities from transforming in dungeons

---
+++ src/main/kotlin/de/fuballer/mcendgame/component/dungeon/tweaks/entity_convert/EntityTransformService.kt
@@ -0,0 +1,16 @@
+package de.fuballer.mcendgame.component.dungeon.tweaks.entity_convert
+
+import de.fuballer.mcendgame.framework.annotation.Component
+import de.fuballer.mcendgame.util.WorldUtil
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityTransformEvent
+
+@Component
+class EntityTransformService : Listener {
+    @EventHandler
+    fun onEntityTransform(event: EntityTransformEvent) {
+        if (WorldUtil.isNotDungeonWorld(event.entity.world)) return
+        event.isCancelled = true
+    }
+}
