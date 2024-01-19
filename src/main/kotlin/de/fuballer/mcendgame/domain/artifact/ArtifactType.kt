2b1ab294e0bbefabf852fd1bf62816b06f8732aa
Moewe342 <fynn.choinowski@gmail.com>
2024-01-20 00:22:49 +0100

    mutable

--- src/main/kotlin/de/fuballer/mcendgame/domain/artifact/ArtifactType.kt
+++ src/main/kotlin/de/fuballer/mcendgame/domain/artifact/ArtifactType.kt
@@ -8,7 +8,7 @@ enum class ArtifactType(
     SLOW_WHEN_HIT(
         "Artifact of Hinder",
         "Slows nearby enemies with slowness %s\\for %s seconds when you're hit",
-        mutableMapOf(
+        mapOf(
             // TIER = AMPLIFIER + 1, DURATION in seconds
             0 to listOf(1.0, 2.0),
             1 to listOf(1.0, 3.5),
@@ -19,7 +19,7 @@ enum class ArtifactType(
     HEAL_ON_BLOCK(
         "Artifact of Recoup",
         "%s%% chance to heal %s health\\when you block damage with a shield\\(%s seconds cooldown)",
-        mutableMapOf(
+        mapOf(
             // TIER = CHANCE in %
             0 to listOf(50.0, 1.0, 1.5),
             1 to listOf(75.0, 1.0, 1.5),
@@ -30,7 +30,7 @@ enum class ArtifactType(
     WOLF_COMPANION(
         "Artifact of Company",
         "You are accompanied by %s invincible wolf/s.\\Your wolfs gains permanent strength %s",
-        mutableMapOf(
+        mapOf(
             //TIER = COUNT, STRENGTH AMPLIFIER + 1
             0 to listOf(1.0, 3.0),
             1 to listOf(2.0, 3.0),
@@ -41,7 +41,7 @@ enum class ArtifactType(
     INC_DMG_PER_MISSING_HEALTH(
         "Artifact of Berserk",
         "You deal %s%% increased damage\\for each missing heart",
-        mutableMapOf(
+        mapOf(
             //TIER = INCREASED DAMAGE in % (per 2 health)
             0 to listOf(4.0),
             1 to listOf(6.0),
@@ -52,7 +52,7 @@ enum class ArtifactType(
     INC_DMG_AGAINST_FULL_LIFE(
         "Artifact of Commencement",
         "You deal %s%% increased damage\\against enemies with full health",
-        mutableMapOf(
+        mapOf(
             //TIER = INCREASED DAMAGE in %
             0 to listOf(50.0),
             1 to listOf(75.0),
@@ -63,7 +63,7 @@ enum class ArtifactType(
     MOVEMENT_SPEED(
         "Artifact of Swiftness",
         "You have %s%% increased base movement speed",
-        mutableMapOf(
+        mapOf(
             //TIER = INCREASED BASE SPEED in %
             0 to listOf(5.0),
             1 to listOf(10.0),
@@ -74,7 +74,7 @@ enum class ArtifactType(
     ATTACK_SPEED(
         "Artifact of Frenzy",
         "You have %s additional attacks per second",
-        mutableMapOf(
+        mapOf(
             //TIER = ATTACK SPEED (additional attacks/second) (base: axe 0.8, sword 1.6, hoe 4.0)
             0 to listOf(0.1),
             1 to listOf(0.2),
@@ -85,7 +85,7 @@ enum class ArtifactType(
     ATTACK_DAMAGE(
         "Artifact of Force",
         "You deal %s additional base damage\\with melee attacks",
-        mutableMapOf(
+        mapOf(
             //TIER = BASE ATTACK DAMAGE
             0 to listOf(1.0),
             1 to listOf(2.0),
@@ -96,7 +96,7 @@ enum class ArtifactType(
     MAX_HEALTH(
         "Artifact of Thickness", //name chosen by xX20Erik01Xx
         "You have %s more health",
-        mutableMapOf(
+        mapOf(
             //TIER = HEALTH
             0 to listOf(1.0),
             1 to listOf(2.0),
@@ -107,7 +107,7 @@ enum class ArtifactType(
     ADDITIONAL_ARROWS(
         "Artifact of Barrage",
         "You shoot %s additional arrows\\dealing %s%% of the damage.\\Your arrows don't damage other players",
-        mutableMapOf(
+        mapOf(
             //TIER = ADDITIONAL ARROWS, dealing % of DAMAGE
             0 to listOf(2.0, 30.0),
             1 to listOf(2.0, 60.0),
@@ -118,7 +118,7 @@ enum class ArtifactType(
     BOW_DAMAGE(
         "Artifact of Impact",
         "Your arrows deal %s%% increased damage.",
-        mutableMapOf(
+        mapOf(
             //TIER = increased DAMAGE in %
             0 to listOf(15.0),
             1 to listOf(30.0),
@@ -128,8 +128,8 @@ enum class ArtifactType(
     ),
     EFFECT_STEAL(
         "Artifact of Rampage",
-        "You have a %s%% chance to steal 1 effect\\for %s seconds when killing an enemy.\\The maximum stolen effect level is %s",
-        mutableMapOf(
+        "You have a %s%% chance to steal one effect\\for %s seconds when killing an enemy.\\The maximum stolen effect level is %s",
+        mapOf(
             //TIER = CHANCE IN %, DURATION, MAX AMPLIFIER + 1
             0 to listOf(25.0, 30.0, 1.0),
             1 to listOf(25.0, 30.0, 2.0),
@@ -140,12 +140,12 @@ enum class ArtifactType(
     TAUNT(
         "Artifact of Disturbance",
         "You have a %s%% chance\\to taunt an enemy on hit",
-        mutableMapOf(
+        mapOf(
             //TIER = CHANCE IN %
             0 to listOf(25.0),
             1 to listOf(50.0),
             2 to listOf(75.0),
             3 to listOf(100.0),
         )
-    )
+    ),
 }
