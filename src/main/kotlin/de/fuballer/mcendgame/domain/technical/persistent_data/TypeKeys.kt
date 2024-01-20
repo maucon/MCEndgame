5756eee75df9d49be96c289f87228770a843808f
maucon <maucon13@gmail.com>
2024-01-20 15:43:31 +0100

    update namespacedkeys

--- src/main/kotlin/de/fuballer/mcendgame/domain/technical/persistent_data/TypeKeys.kt
+++ src/main/kotlin/de/fuballer/mcendgame/domain/technical/persistent_data/TypeKeys.kt
@@ -10,26 +10,26 @@ import org.bukkit.persistence.PersistentDataType
 
 object TypeKeys {
     // entities
-    val DROP_EQUIPMENT = TypeKey(createNamespacedKey("drop-equipment"), PersistentDataType.BOOLEAN)
-    val MAP_TIER = TypeKey(createNamespacedKey("map-tier"), PersistentDataType.INTEGER)
-    val IS_MINION = TypeKey(createNamespacedKey("is-minion"), PersistentDataType.BOOLEAN)
-    val HIDE_EQUIPMENT = TypeKey(createNamespacedKey("hide-equipment"), PersistentDataType.BOOLEAN)
-    val CUSTOM_ENTITY_TYPE = TypeKey(createNamespacedKey("custom-entity-type"), PersistentObjectClass(CustomEntityType::class.java))
-    val IS_ENEMY = TypeKey(createNamespacedKey("is-enemy"), PersistentDataType.BOOLEAN)
+    val DROP_EQUIPMENT = TypeKey(createNamespacedKey("drop_equipment"), PersistentDataType.BOOLEAN)
+    val MAP_TIER = TypeKey(createNamespacedKey("map_tier"), PersistentDataType.INTEGER)
+    val IS_MINION = TypeKey(createNamespacedKey("is_minion"), PersistentDataType.BOOLEAN)
+    val HIDE_EQUIPMENT = TypeKey(createNamespacedKey("hide_equipment"), PersistentDataType.BOOLEAN)
+    val CUSTOM_ENTITY_TYPE = TypeKey(createNamespacedKey("custom_entity_type"), PersistentObjectClass(CustomEntityType::class.java))
+    val IS_ENEMY = TypeKey(createNamespacedKey("is_enemy"), PersistentDataType.BOOLEAN)
     val MINIONS = TypeKey(createNamespacedKey("minions"), PersistentUuidSet)
 
     // player
-    val LAST_MAP_DEVICE = TypeKey(createNamespacedKey("last-map-device"), PersistentUuid)
+    val LAST_MAP_DEVICE = TypeKey(createNamespacedKey("last_map_device"), PersistentUuid)
     val ARTIFACTS = TypeKey(createNamespacedKey("artifacts"), PersistentArtifactList)
-    val HEAL_ON_BLOCK_ARTIFACT_ACTIVATION = TypeKey(createNamespacedKey("heal-on-block-artifact-activation"), PersistentDataType.LONG)
+    val HEAL_ON_BLOCK_ARTIFACT_ACTIVATION = TypeKey(createNamespacedKey("heal_on_block_artifact_activation"), PersistentDataType.LONG)
 
     // items
-    val CUSTOM_ITEM_TYPE = TypeKey(createNamespacedKey("custom-entity-type"), PersistentObjectClass(CustomItemType::class.java))
+    val CUSTOM_ITEM_TYPE = TypeKey(createNamespacedKey("custom_entity_type"), PersistentObjectClass(CustomItemType::class.java))
     val ATTRIBUTES = TypeKey(createNamespacedKey("attributes"), PersistentRolledAttributeList)
     val UNMODIFIABLE = TypeKey(createNamespacedKey("unmodifiable"), PersistentDataType.BOOLEAN)
-    val CORRUPTION_ROUNDS = TypeKey(createNamespacedKey("corruption-rounds"), PersistentDataType.INTEGER)
-    val MAP_DEVICE = TypeKey(createNamespacedKey("map-device"), PersistentDataType.BOOLEAN)
-    val MAP_DEVICE_ACTION = TypeKey(createNamespacedKey("map-device-action"), PersistentEnum(MapDeviceAction::class))
+    val CORRUPTION_ROUNDS = TypeKey(createNamespacedKey("corruption_rounds"), PersistentDataType.INTEGER)
+    val MAP_DEVICE = TypeKey(createNamespacedKey("map_device"), PersistentDataType.BOOLEAN)
+    val MAP_DEVICE_ACTION = TypeKey(createNamespacedKey("map_device_action"), PersistentEnum(MapDeviceAction::class))
     val ARTIFACT = TypeKey(createNamespacedKey("artifact"), PersistentArtifact)
 
     class TypeKey<T>(
