d1d784a9ecc6975c373549bdcb928bc3d8631d77
Moewe342 <fynn.choinowski@gmail.com>
2024-02-19 21:09:19 +0100

    melee on melee

--- src/main/kotlin/de/fuballer/mcendgame/component/attribute/effects/DisableMeleeEffectService.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/attribute/effects/DisableMeleeEffectService.kt
@@ -8,10 +8,12 @@ import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
 
 @Component
-class DisableMeleeEffectService  : Listener {
+class DisableMeleeEffectService : Listener {
     @EventHandler(ignoreCancelled = true)
     fun on(event: DamageCalculationEvent) {
-        if (event.cause == EntityDamageEvent.DamageCause.PROJECTILE) return
+        if (event.cause != EntityDamageEvent.DamageCause.ENTITY_ATTACK
+            && event.cause != EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK
+        ) return
 
         if (!event.customDamagerAttributes.containsKey(AttributeType.DISABLE_MELEE)) return
 
