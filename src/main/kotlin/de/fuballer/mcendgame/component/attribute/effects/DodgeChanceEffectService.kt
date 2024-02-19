79686c3f6b6dd575e206ff7b4467076d54889b23
Moewe342 <fynn.choinowski@gmail.com>
2024-02-19 21:12:36 +0100

    only dodge dodgeable

--- src/main/kotlin/de/fuballer/mcendgame/component/attribute/effects/DodgeChanceEffectService.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/attribute/effects/DodgeChanceEffectService.kt
@@ -5,12 +5,18 @@ import de.fuballer.mcendgame.component.damage.DamageCalculationEvent
 import de.fuballer.mcendgame.framework.annotation.Component
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageEvent
 import kotlin.random.Random
 
 @Component
 class DodgeChanceEffectService : Listener {
     @EventHandler(ignoreCancelled = true)
     fun on(event: DamageCalculationEvent) {
+        if (event.cause != EntityDamageEvent.DamageCause.PROJECTILE
+            && event.cause != EntityDamageEvent.DamageCause.ENTITY_ATTACK
+            && event.cause != EntityDamageEvent.DamageCause.ENTITY_SWEEP_ATTACK
+        ) return
+
         val dodgeAttributes = event.customDamagedAttributes[AttributeType.DODGE_CHANCE] ?: return
 
         val dodgeChance = dodgeAttributes.sum()
