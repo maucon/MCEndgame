71479bb04503ea51f49baa2a8434e835f0ddbe0f
Moewe342 <fynn.choinowski@gmail.com>
2024-02-22 23:23:52 +0100

    make vitality surge a chestplate

--- src/main/kotlin/de/fuballer/mcendgame/component/item/custom_item/types/VitalitySurgeItemType.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/item/custom_item/types/VitalitySurgeItemType.kt
@@ -3,13 +3,13 @@ package de.fuballer.mcendgame.component.item.custom_item.types
 import de.fuballer.mcendgame.component.attribute.AttributeType
 import de.fuballer.mcendgame.component.attribute.RollableAttribute
 import de.fuballer.mcendgame.component.item.custom_item.CustomItemType
-import de.fuballer.mcendgame.component.item.equipment.armor.Leggings
+import de.fuballer.mcendgame.component.item.equipment.armor.Chestplate
 import de.fuballer.mcendgame.util.PluginUtil
 
 object VitalitySurgeItemType : CustomItemType {
     override val key = PluginUtil.createNamespacedKey("vitality_surge")
     override val customName = "Vitality Surge"
-    override val equipment = Leggings.NETHERITE
+    override val equipment = Chestplate.NETHERITE
     override val usesEquipmentBaseStats = true
     override val attributes = listOf(
         RollableAttribute(AttributeType.ABSORPTION_ON_HIGH_DAMAGE_TAKEN, 5.0, 8.0),
