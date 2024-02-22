1bd29f25cf8b9778e89e3dd391e16bd1ba8f2bff
Moewe342 <fynn.choinowski@gmail.com>
2024-02-22 23:42:10 +0100

    reformat #2

--- src/main/kotlin/de/fuballer/mcendgame/component/attribute/AttributeType.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/attribute/AttributeType.kt
@@ -80,17 +80,30 @@ enum class AttributeType(
 
     // custom
     HEALTH_SCALED_SIZE({ " ${DECIMAL_FORMAT.format(it)}% increased size per 2 extra health" }),
+
     DISABLE_MELEE({ " Cannot deal melee damage" }),
+
     DODGE_CHANCE({ " ${DECIMAL_FORMAT.format(it * 100)}% chance to dodge hits" }),
+
     TWINFIRE_DUAL_WIELD({ " ${DECIMAL_FORMAT.format(it * 100)}% more damage while dual wielding Twinfire" }),
+
     ABSORPTION_ON_HIGH_DAMAGE_TAKEN({ " When taking at least ${DECIMAL_FORMAT.format(it)} damage, gain Absorption II" }),
+
     STEALTH({ " Cannot be targeted by enemies facing away" }),
+
     BACKSTAB({ " ${DECIMAL_FORMAT.format(it * 100)}% more damage with attacks from behind" }),
+
     ADDITIONAL_ARROWS({ " Shoot 2 additional arrows dealing ${DECIMAL_FORMAT.format(it)}% damage" }),
+
     EFFECT_GAIN({ " ${DECIMAL_FORMAT.format(it * 100)}% chance to gain a random positive effect on kill" }),
+
     MORE_DAMAGE_AGAINST_FULL_LIFE({ " ${DECIMAL_FORMAT.format(it * 100)}% more damage against full life enemies" }),
+
     HEAL_ON_BLOCK({ " Heal ${DECIMAL_FORMAT.format(it)} health on block, with a cooldown of 7 seconds" }),
+
     INC_DAMAGE_PER_MISSING_HEART({ " ${DECIMAL_FORMAT.format(it * 100)}% increased damage per missing heart" }),
+
     SLOW_ON_HIT({ " Affect nearby enemies with Slowness II for ${DECIMAL_FORMAT.format(it)} seconds on hit" }),
+
     TAUNT({ "+${DECIMAL_FORMAT.format(it * 100)}% chance to taunt an enemy on hit" }),
 }
