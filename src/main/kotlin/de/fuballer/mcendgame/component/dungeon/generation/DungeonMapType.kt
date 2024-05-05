4ced2200e9fb4c8f75ab8e53bc7e21947a6f487e
Moewe342 <fynn.choinowski@gmail.com>
2024-05-05 17:12:53 +0200

    add new room to list

--- src/main/kotlin/de/fuballer/mcendgame/component/dungeon/generation/DungeonMapType.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/dungeon/generation/DungeonMapType.kt
@@ -9,6 +9,7 @@ private val rooms = listOf(
     RoomTypeLoader.load("stronghold/stronghold_large_0011_0", 10),
     RoomTypeLoader.load("stronghold/stronghold_large_1010_0", 10),
     RoomTypeLoader.load("stronghold/stronghold_large_1010_1", 10),
+    RoomTypeLoader.load("stronghold/stronghold_large_1010_2", 10),
     RoomTypeLoader.load("stronghold/stronghold_medium_0110_0", 10),
     RoomTypeLoader.load("stronghold/stronghold_medium_0110_1", 10),
     RoomTypeLoader.load("stronghold/stronghold_medium_1110_0", 10),
