139b5ce97167cf79228843462701d3a8011fa09a
Moewe342 <fynn.choinowski@gmail.com>
2024-05-22 21:22:53 +0200

    necromancer default attack

--- src/main/kotlin/de/fuballer/mcendgame/component/custom_entity/types/necromancer/NecromancerService.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/custom_entity/types/necromancer/NecromancerService.kt
@@ -1,16 +1,12 @@
 package de.fuballer.mcendgame.component.custom_entity.types.necromancer
 
-import de.fuballer.mcendgame.component.custom_entity.types.deathball.DeathBallEntityType
-import de.fuballer.mcendgame.configuration.PluginConfiguration
 import de.fuballer.mcendgame.framework.annotation.Component
 import de.fuballer.mcendgame.util.EnemyUtil
+import de.fuballer.mcendgame.util.PluginUtil.runTaskLater
 import de.fuballer.mcendgame.util.extension.EntityExtension.getCustomEntityType
 import de.fuballer.mcendgame.util.extension.EventExtension.cancel
 import org.bukkit.Sound
-import org.bukkit.entity.Arrow
-import org.bukkit.entity.EntityType
-import org.bukkit.entity.LivingEntity
-import org.bukkit.entity.Snowball
+import org.bukkit.entity.*
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityShootBowEvent
@@ -23,22 +19,26 @@ class NecromancerService : Listener {
     @EventHandler
     fun on(event: EntityShootBowEvent) {
         if (event.entity.getCustomEntityType() != NecromancerEntityType) return
+        val necromancer = event.entity as? Creature ?: return
 
-        event.entity.swingMainHand()
+        necromancer.swingMainHand()
 
         val arrow = event.projectile as? Arrow ?: return
-        ShootDeathBall(event.entity, arrow).runTaskLater(PluginConfiguration.INSTANCE, baseAttackDelay)
+        val target = necromancer.target ?: return
+
+        ShootWitherSkull(necromancer, arrow, target).runTaskLater(baseAttackDelay)
 
         event.cancel()
     }
 
-    private class ShootDeathBall(
+    private class ShootWitherSkull(
         private val necromancer: LivingEntity,
-        private val arrow: Arrow
+        private val arrow: Arrow,
+        private val target: LivingEntity
     ) : BukkitRunnable() {
         override fun run() {
-            val deathBall = EnemyUtil.shootCustomProjectile(necromancer, arrow, EntityType.SNOWBALL, Sound.ITEM_FIRECHARGE_USE) as Snowball
-            deathBall.customName = DeathBallEntityType.customName
+            val witherSkull = EnemyUtil.shootCustomProjectile(necromancer, arrow, target, EntityType.WITHER_SKULL, Sound.ITEM_FIRECHARGE_USE) as WitherSkull
+            witherSkull.direction = witherSkull.velocity
         }
     }
 }
--- src/main/kotlin/de/fuballer/mcendgame/util/EnemyUtil.kt
+++ src/main/kotlin/de/fuballer/mcendgame/util/EnemyUtil.kt
@@ -8,6 +8,7 @@ import org.bukkit.entity.AbstractArrow
 import org.bukkit.entity.EntityType
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Projectile
+import org.bukkit.util.Vector
 
 object EnemyUtil {
     fun shootCustomProjectile(
@@ -24,6 +25,28 @@ object EnemyUtil {
 
         val addedDamage = projectile.getAddedBaseDamage() ?: 0.0
         val damage = projectile.damage + addedDamage
+        newProjectile.setAddedBaseDamage(damage)
+
+        return newProjectile
+    }
+
+    fun shootCustomProjectile(
+        shooter: LivingEntity,
+        oldProjectile: AbstractArrow,
+        target: LivingEntity,
+        newProjectileType: EntityType,
+        sound: Sound
+    ): Projectile {
+        val newProjectile = shooter.world.spawnEntity(oldProjectile.location, newProjectileType, false) as Projectile
+        newProjectile.shooter = shooter
+
+        val targetPosition = target.eyeLocation.toVector().add(target.location.toVector()).divide(Vector(2, 2, 2))
+        newProjectile.velocity = targetPosition.subtract(oldProjectile.location.toVector()).normalize()
+
+        shooter.world.playSound(shooter.location, sound, SoundCategory.HOSTILE, 1f, 1f)
+
+        val addedDamage = oldProjectile.getAddedBaseDamage() ?: 0.0
+        val damage = oldProjectile.damage + addedDamage
         newProjectile.setAddedBaseDamage(damage)
 
         return newProjectile
