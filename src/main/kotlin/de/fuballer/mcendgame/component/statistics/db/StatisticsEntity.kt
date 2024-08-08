b139caeda35b8c9215a798d3cfb1846a092ba82d
maucon <maucon13@gmail.com>
2024-08-08 23:11:42 +0200

    update statistics

--- src/main/kotlin/de/fuballer/mcendgame/component/statistics/db/StatisticsEntity.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/statistics/db/StatisticsEntity.kt
@@ -2,6 +2,8 @@ package de.fuballer.mcendgame.component.statistics.db
 
 import de.fuballer.mcendgame.component.statistics.StatisticsSettings
 import de.fuballer.mcendgame.framework.stereotype.Entity
+import de.fuballer.mcendgame.util.TextComponent
+import net.kyori.adventure.text.Component
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.BookMeta
@@ -27,51 +29,44 @@ data class StatisticsEntity(
         bookMeta.title = StatisticsSettings.STATISTICS_BOOK_TITLE
 
         val pages = getPages()
-        for (page in pages)
-            bookMeta.addPage(page)
+        bookMeta.addPages(*pages)
 
         book.itemMeta = bookMeta
 
         return book
     }
 
-    private fun getPages(): List<String> {
-        val pages: MutableList<String> = mutableListOf()
+    private fun getPages(): Array<Component> {
+        val pages = mutableListOf<Component>()
 
-        val page1 = createPage(
-            listOf(
-                StatisticsSettings.STATISTICS_BOOK_DUNGEONS_HEADLINE,
-                "",
-                StatisticsSettings.DUNGEONS_OPENED_TEXT,
-                "   $dungeonsOpened",
-                StatisticsSettings.DUNGEONS_COMPLETED_TEXT,
-                "   $dungeonsCompleted",
-                StatisticsSettings.HIGHEST_DUNGEONS_COMPLETED_TEXT,
-                "   $highestCompletedDungeon",
-            )
+        val page1 = Component.textOfChildren(
+            StatisticsSettings.STATISTICS_BOOK_DUNGEONS_HEADLINE,
+            TextComponent.empty(),
+            StatisticsSettings.DUNGEONS_OPENED_TEXT,
+            TextComponent.create("   $dungeonsOpened", StatisticsSettings.STATISTICS_BOOK_TEXT_COLOR),
+            StatisticsSettings.DUNGEONS_COMPLETED_TEXT,
+            TextComponent.create("   $dungeonsCompleted", StatisticsSettings.STATISTICS_BOOK_TEXT_COLOR),
+            StatisticsSettings.HIGHEST_DUNGEONS_COMPLETED_TEXT,
+            TextComponent.create("   $highestCompletedDungeon", StatisticsSettings.STATISTICS_BOOK_TEXT_COLOR),
         )
         pages.add(page1)
 
-        val page2 = createPage(
-            listOf(
-                StatisticsSettings.STATISTICS_BOOK_COMBAT_HEADLINE,
-                "",
-                StatisticsSettings.TOTAL_KILLS_TEXT,
-                "   $totalKills",
-                StatisticsSettings.BOSS_KILLS_TEXT,
-                "   $bossKills",
-                StatisticsSettings.LOOT_GOBLINS_KILLS_TEXT,
-                "   $lootGoblinKills",
-                StatisticsSettings.ELITE_KILLS_TEXT,
-                "   $eliteKills",
-                StatisticsSettings.DEATHS_TEXT,
-                "   $deaths",
-            )
+        val page2 = Component.textOfChildren(
+            StatisticsSettings.STATISTICS_BOOK_COMBAT_HEADLINE,
+            TextComponent.empty(),
+            StatisticsSettings.TOTAL_KILLS_TEXT,
+            TextComponent.create("   $totalKills", StatisticsSettings.STATISTICS_BOOK_TEXT_COLOR),
+            StatisticsSettings.BOSS_KILLS_TEXT,
+            TextComponent.create("   $bossKills", StatisticsSettings.STATISTICS_BOOK_TEXT_COLOR),
+            StatisticsSettings.LOOT_GOBLINS_KILLS_TEXT,
+            TextComponent.create("   $lootGoblinKills", StatisticsSettings.STATISTICS_BOOK_TEXT_COLOR),
+            StatisticsSettings.ELITE_KILLS_TEXT,
+            TextComponent.create("   $eliteKills", StatisticsSettings.STATISTICS_BOOK_TEXT_COLOR),
+            StatisticsSettings.DEATHS_TEXT,
+            TextComponent.create("   $deaths", StatisticsSettings.STATISTICS_BOOK_TEXT_COLOR),
         )
         pages.add(page2)
 
-        return pages
+        return pages.toTypedArray()
     }
-
-    private fun createPage(lines: List<String>) = lines.joinToString("\n")
 }
