761b76391489a18659739f308136f958d996e5ff
maucon <maucon13@gmail.com>
2024-08-17 14:47:00 +0200

    update dungeon progress component use

--- src/main/kotlin/de/fuballer/mcendgame/component/dungeon/progress/PlayerDungeonProgressSettings.kt
+++ src/main/kotlin/de/fuballer/mcendgame/component/dungeon/progress/PlayerDungeonProgressSettings.kt
@@ -1,6 +1,9 @@
 package de.fuballer.mcendgame.component.dungeon.progress
 
-import org.bukkit.ChatColor
+import de.fuballer.mcendgame.util.TextComponent
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextDecoration
 
 object PlayerDungeonProgressSettings {
     const val DUNGEON_LEVEL_INCREASE_THRESHOLD = 3
@@ -8,17 +11,31 @@ object PlayerDungeonProgressSettings {
 
     const val MAX_DUNGEON_TIER = 300 // only for the set progress command
 
-    val PLAYER_NO_PROGRESS_MESSAGE = "${ChatColor.RED}Player has no dungeon progress"
+    val PLAYER_NO_PROGRESS_MESSAGE = TextComponent.error("Player has no dungeon progress")
 
-    fun getDungeonProgressMessage(playerName: String, tier: Int, progress: Int) =
-        "${ChatColor.AQUA}${ChatColor.ITALIC}$playerName${ChatColor.RESET}${ChatColor.AQUA} is Tier: $tier${ChatColor.GRAY},${ChatColor.LIGHT_PURPLE} Progress: ${progress}/$DUNGEON_LEVEL_INCREASE_THRESHOLD${ChatColor.RESET}"
+    fun getDungeonProgressMessage(playerName: String, tier: Int, progress: Int) = Component.textOfChildren(
+        TextComponent.create(playerName, NamedTextColor.AQUA).decorate(TextDecoration.ITALIC),
+        TextComponent.create(" is Tier: $tier", NamedTextColor.AQUA),
+        getProgressMessageComponent(progress)
+    )
 
-    private val dungeonCompleteMessage = "${ChatColor.BOLD}${ChatColor.GOLD}Dungeon completed! "
-    val NO_PROGRESS_MESSAGE = "$dungeonCompleteMessage${ChatColor.RESET}${ChatColor.RED}Dungeon tier is too low to grant progress!"
+    private val dungeonCompleteMessage = TextComponent.create("Dungeon completed! ", NamedTextColor.GOLD).decorate(TextDecoration.BOLD)
+    val NO_PROGRESS_MESSAGE = dungeonCompleteMessage.append(TextComponent.error("Dungeon tier is too low to grant progress!"))
 
-    fun getProgressMessage(tier: Int, progress: Int) =
-        "$dungeonCompleteMessage${ChatColor.RESET}${ChatColor.AQUA}Tier: $tier${ChatColor.GRAY},${ChatColor.LIGHT_PURPLE} Progress: $progress/${DUNGEON_LEVEL_INCREASE_THRESHOLD}${ChatColor.RESET}"
+    fun getProgressMessage(tier: Int, progress: Int) = Component.textOfChildren(
+        dungeonCompleteMessage,
+        TextComponent.create("You are now Tier: $tier", NamedTextColor.AQUA),
+        getProgressMessageComponent(progress)
+    )
 
-    fun getRegressMessage(tier: Int, progress: Int) =
-        "${ChatColor.BOLD}${ChatColor.RED}You died! ${ChatColor.RESET}${ChatColor.AQUA}You are now Tier: $tier${ChatColor.GRAY},${ChatColor.LIGHT_PURPLE} Progress: ${progress}/${DUNGEON_LEVEL_INCREASE_THRESHOLD}${ChatColor.RESET}"
+    fun getRegressMessage(tier: Int, progress: Int) = Component.textOfChildren(
+        TextComponent.create("You died! ", NamedTextColor.RED).decorate(TextDecoration.BOLD),
+        TextComponent.create("You are now Tier: $tier", NamedTextColor.AQUA),
+        getProgressMessageComponent(progress)
+    )
+
+    private fun getProgressMessageComponent(progress: Int) = Component.textOfChildren(
+        TextComponent.create(",", NamedTextColor.GRAY),
+        TextComponent.create(" Progress: $progress/${DUNGEON_LEVEL_INCREASE_THRESHOLD}", NamedTextColor.LIGHT_PURPLE)
+    )
 }
