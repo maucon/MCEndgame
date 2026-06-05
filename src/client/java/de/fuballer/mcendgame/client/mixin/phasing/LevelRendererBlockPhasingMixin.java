94707c0988baec86112fef3710a0145650d865ea
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-06-06 00:59:35 +0200

    fix level renderer block phasing

--- src/client/java/de/fuballer/mcendgame/client/mixin/phasing/LevelRendererBlockPhasingMixin.java
+++ src/client/java/de/fuballer/mcendgame/client/mixin/phasing/LevelRendererBlockPhasingMixin.java
@@ -3,7 +3,7 @@ package de.fuballer.mcendgame.client.mixin.phasing;
 import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import com.mojang.blaze3d.framegraph.FrameGraphBuilder;
 import de.fuballer.mcendgame.main.util.extension.EntityExtension;
-import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.state.level.CameraRenderState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,19 +13,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LevelRenderer.class)
 public class LevelRendererBlockPhasingMixin {
-//    @Inject( FIXME
-//            method = "addSkyPass",
-//            at = @At("HEAD"),
-//            cancellable = true
-//    )
-//    void renderSky(
-//            FrameGraphBuilder frame,
-//            CameraRenderState cameraState,
-//            GpuBufferSlice skyFog,
-//            CallbackInfo ci
-//    ) {
-//        var entity = camera.entity();
-//        if (!EntityExtension.INSTANCE.isBlockPhasingAtEyes(entity)) return;
-//        ci.cancel();
-//    }
+    @Inject(
+            method = "addSkyPass",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    void renderSky(
+            FrameGraphBuilder frame,
+            CameraRenderState cameraState,
+            GpuBufferSlice skyFog,
+            CallbackInfo ci
+    ) {
+        var player = Minecraft.getInstance().player;
+        if (player == null) return;
+        if (!EntityExtension.INSTANCE.isBlockPhasingAtEyes(player)) return;
+        ci.cancel();
+    }
 }
