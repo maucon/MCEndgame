eb6ac02f240e9c61cdf3d301cea6a8643bc5e0e5
maucon <maucon13@gmail.com>
2026-08-07 12:30:47 +0200

    run client tests

--- src/gametest/kotlin/de/fuballer/mcendgame/client/MCEndgameClientGameTest.kt
+++ src/gametest/kotlin/de/fuballer/mcendgame/client/MCEndgameClientGameTest.kt
@@ -2,13 +2,24 @@ package de.fuballer.mcendgame.client
 
 import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest
 import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext
+import net.minecraft.world.phys.Vec3
 
 @Suppress("UnstableApiUsage")
 class MCEndgameClientGameTest : FabricClientGameTest {
     override fun runTest(context: ClientGameTestContext) {
         context.worldBuilder().create().use { singleplayer ->
             singleplayer.clientLevel.waitForChunksRender()
-            context.takeScreenshot("example-mod-singleplayer-test")
+            val pos = singleplayer.server.computeOnServer<Vec3, RuntimeException> { server ->
+                val player = server.playerList.players.first()
+                player.position()
+            }
+
+            singleplayer.server.runCommand(
+                "summon mcendgame:arachne ${pos.x} ${pos.y} ${pos.z + 10}"
+            )
+            context.waitTicks(5)
+
+            context.takeScreenshot("arachne")
         }
     }
 }
