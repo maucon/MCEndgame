ac3ed89840bfe9ffa98180a7c584fa866444a152
maucon <maucon13@gmail.com>
2026-08-07 20:00:57 +0200

    add tests

--- src/gametest/kotlin/de/fuballer/mcendgame/test/client/MCEndgameClientGameTest.kt
+++ src/gametest/kotlin/de/fuballer/mcendgame/test/client/MCEndgameClientGameTest.kt
@@ -2,22 +2,33 @@ package de.fuballer.mcendgame.test.client
 
 import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest
 import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext
-import net.minecraft.world.phys.Vec3
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.resources.Identifier
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.entity.EntitySpawnReason
+import org.slf4j.LoggerFactory
 
 @Suppress("UnstableApiUsage")
 class MCEndgameClientGameTest : FabricClientGameTest {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(this::class.java.simpleName)
+    }
+
     override fun runTest(context: ClientGameTestContext) {
-        // TODO disable mcendgame telemetry
         context.worldBuilder().create().use { singleplayer ->
             singleplayer.clientLevel.waitForChunksRender()
-            val pos = singleplayer.server.computeOnServer<Vec3, RuntimeException> { server ->
-                val player = server.playerList.players.first()
-                player.position()
+
+            val level = singleplayer.server.computeOnServer<ServerLevel, RuntimeException> { server ->
+                server.overworld()
             }
 
-            singleplayer.server.runCommand(
-                "summon mcendgame:arachne ${pos.x} ${pos.y} ${pos.z + 10}"
-            )
+            for (entityType in BuiltInRegistries.ENTITY_TYPE) {
+                val id = BuiltInRegistries.ENTITY_TYPE.getKey(entityType)
+                if (id == Identifier.withDefaultNamespace("player")) continue
+
+                LOGGER.info("Spawning {}", id)
+                entityType.create(level, EntitySpawnReason.MOB_SUMMONED)
+            }
             context.waitTicks(5)
 
             context.takeScreenshot("arachne")
