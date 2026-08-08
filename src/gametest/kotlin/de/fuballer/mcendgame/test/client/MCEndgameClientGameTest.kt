51236416696f31f02243d7100d8525e63c02978b
maucon <55910883+maucon@users.noreply.github.com>
2026-08-08 13:36:10 +0200

    Game Tests (#304)

---
+++ src/gametest/kotlin/de/fuballer/mcendgame/test/client/MCEndgameClientGameTest.kt
@@ -0,0 +1,39 @@
+package de.fuballer.mcendgame.test.client
+
+import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest
+import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.resources.Identifier
+import net.minecraft.world.phys.Vec3
+import org.slf4j.LoggerFactory
+
+@Suppress("UnstableApiUsage")
+class MCEndgameClientGameTest : FabricClientGameTest {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(this::class.java.simpleName)
+    }
+
+    override fun runTest(context: ClientGameTestContext) {
+        context.worldBuilder().create().use { singleplayer ->
+            singleplayer.clientLevel.waitForChunksRender()
+
+            val pos = singleplayer.server.computeOnServer<Vec3, RuntimeException> { server ->
+                val player = server.playerList.players.first()
+                player.position()
+            }
+
+            for (entityType in BuiltInRegistries.ENTITY_TYPE) {
+                val id = BuiltInRegistries.ENTITY_TYPE.getKey(entityType)
+                if (id == Identifier.withDefaultNamespace("player")) continue
+
+                LOGGER.info("Spawning {}", id)
+                singleplayer.server.runCommand(
+                    "summon $id ${pos.x} ${pos.y} ${pos.z + 10}"
+                )
+            }
+            context.waitTicks(5)
+
+            context.takeScreenshot("all_entities")
+        }
+    }
+}
---
+++ src/gametest/kotlin/de/fuballer/mcendgame/test/main/MCEndgameGameTest.kt
@@ -0,0 +1,34 @@
+package de.fuballer.mcendgame.test.main
+
+import net.fabricmc.fabric.api.gametest.v1.GameTest
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.gametest.framework.GameTestHelper
+import net.minecraft.resources.Identifier
+import net.minecraft.world.level.block.Blocks
+import org.slf4j.LoggerFactory
+
+class MCEndgameGameTest {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(this::class.java.simpleName)
+    }
+
+    @GameTest
+    fun test(helper: GameTestHelper) {
+        helper.assertBlockPresent(Blocks.AIR, 0, 0, 0)
+
+        helper.succeed()
+    }
+
+    @GameTest
+    fun testSpawnEntity(helper: GameTestHelper) {
+        for (entityType in BuiltInRegistries.ENTITY_TYPE) {
+            val id = BuiltInRegistries.ENTITY_TYPE.getKey(entityType)
+            if (id == Identifier.withDefaultNamespace("player")) continue
+
+            LOGGER.info("Spawning {}", id)
+            helper.spawn(entityType, 0, 0, 0)
+        }
+
+        helper.succeed()
+    }
+}
