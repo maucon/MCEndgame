df6433105767f4589baafab1097a3efe698eac55
maucon <maucon13@gmail.com>
2026-08-08 13:29:25 +0200

    fix test add wiki images

--- src/gametest/kotlin/de/fuballer/mcendgame/test/client/MCEndgameClientGameTest.kt
+++ src/gametest/kotlin/de/fuballer/mcendgame/test/client/MCEndgameClientGameTest.kt
@@ -4,8 +4,7 @@ import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest
 import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.resources.Identifier
-import net.minecraft.server.level.ServerLevel
-import net.minecraft.world.entity.EntitySpawnReason
+import net.minecraft.world.phys.Vec3
 import org.slf4j.LoggerFactory
 
 @Suppress("UnstableApiUsage")
@@ -18,8 +17,9 @@ class MCEndgameClientGameTest : FabricClientGameTest {
         context.worldBuilder().create().use { singleplayer ->
             singleplayer.clientLevel.waitForChunksRender()
 
-            val level = singleplayer.server.computeOnServer<ServerLevel, RuntimeException> { server ->
-                server.overworld()
+            val pos = singleplayer.server.computeOnServer<Vec3, RuntimeException> { server ->
+                val player = server.playerList.players.first()
+                player.position()
             }
 
             for (entityType in BuiltInRegistries.ENTITY_TYPE) {
@@ -27,11 +27,13 @@ class MCEndgameClientGameTest : FabricClientGameTest {
                 if (id == Identifier.withDefaultNamespace("player")) continue
 
                 LOGGER.info("Spawning {}", id)
-                entityType.create(level, EntitySpawnReason.MOB_SUMMONED)
+                singleplayer.server.runCommand(
+                    "summon $id ${pos.x} ${pos.y} ${pos.z + 10}"
+                )
             }
             context.waitTicks(5)
 
-            context.takeScreenshot("arachne")
+            context.takeScreenshot("all_entities")
         }
     }
 }
