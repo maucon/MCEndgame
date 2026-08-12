5d69010cc80ccb404e42338798e10d47f3a94d30
maucon <maucon13@gmail.com>
2026-08-12 20:44:21 +0200

    fix the fix

--- src/main/kotlin/de/fuballer/mcendgame/main/component/damage/calculator/CreeperExplosionCalculator.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/damage/calculator/CreeperExplosionCalculator.kt
@@ -3,11 +3,14 @@ package de.fuballer.mcendgame.main.component.damage.calculator
 import de.fuballer.mcendgame.main.component.damage.DamageCalculationCommand
 import de.fuballer.mcendgame.main.component.damage.DamageUtil
 import de.fuballer.mcendgame.main.component.damage.dealing.ExtendedDamageSource
+import de.fuballer.mcendgame.main.util.extension.DamageTypeExtension.isOf
 import net.minecraft.world.damagesource.DamageSource
+import net.minecraft.world.damagesource.DamageTypes
 import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.monster.Creeper
 
 object CreeperExplosionCalculator : DamageCalculator {
-    override fun isActive(source: DamageSource) = true
+    override fun isActive(source: DamageSource) = source.directEntity is Creeper && source.type().isOf(DamageTypes.PLAYER_EXPLOSION)
 
     override fun calculateAttackDamage(
         originalDamage: Float,
