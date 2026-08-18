16c2e62072077814e64a0e35b60404ecf95bb7bf
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-08-18 02:48:38 +0200

    mage ancient blight not damage creative or spectator players

--- src/main/kotlin/de/fuballer/mcendgame/main/component/status_effect/AncientBlightEffect.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/status_effect/AncientBlightEffect.kt
@@ -4,6 +4,7 @@ import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.effect.MobEffect
 import net.minecraft.world.effect.MobEffectCategory
 import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.player.Player
 import kotlin.math.max
 
 private const val DAMAGE_PER_LEVEL = 1F
@@ -12,6 +13,7 @@ private const val COLOR = 1349140
 class AncientBlightEffect : MobEffect(MobEffectCategory.HARMFUL, COLOR) {
     override fun applyEffectTick(world: ServerLevel, entity: LivingEntity, amplifier: Int): Boolean {
         if (entity.tickCount % 20 != 0) return true
+        if (entity is Player && (entity.isCreative || entity.isSpectator)) return true
         entity.health = max(entity.health - DAMAGE_PER_LEVEL * (amplifier + 1), 0F) // TODO instead use damage system and deal true damage
         return true
     }
