9c01ae8f6982310312568ed5a6bd48643a35d576
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-08-20 00:18:47 +0200

    make beastweaver boss not have random scale

--- src/main/kotlin/de/fuballer/mcendgame/main/component/entity/EntityTypeStats.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/entity/EntityTypeStats.kt
@@ -3,6 +3,8 @@ package de.fuballer.mcendgame.main.component.entity
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.Mob
+import kotlin.math.pow
+import kotlin.random.Random
 
 interface EntityTypeStats {
     val type: EntityType<out Mob>
@@ -18,6 +20,8 @@ interface EntityTypeStats {
         get() = 0.0
     val movementSpeed: Double
     val knockbackResistance: Double
+
+    fun getRandomScale(random: Random): Double = 1.0 + 0.2 * random.nextDouble().pow(3) * if (random.nextBoolean()) 1 else -1
 
     fun applyMisc(entity: LivingEntity) {}
 }
--- src/main/kotlin/de/fuballer/mcendgame/main/component/entity/types/boss/beastweaver/BeastweaverBossStats.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/entity/types/boss/beastweaver/BeastweaverBossStats.kt
@@ -11,6 +11,7 @@ import de.fuballer.mcendgame.main.component.entity.custom.CustomEntities
 import de.fuballer.mcendgame.main.component.entity.custom.entities.beastweaver.BeastweaverEntity
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.LivingEntity
+import java.util.*
 
 object BeastweaverBossStats : EntityTypeStats {
     override val type: EntityType<BeastweaverEntity> = CustomEntities.BEASTWEAVER
@@ -22,6 +23,8 @@ object BeastweaverBossStats : EntityTypeStats {
     override val attackDamage = 10.0
     override val movementSpeed = 0.35
     override val knockbackResistance = 0.8
+
+    override fun getRandomScale(random: Random) = 1.0
 
     override fun applyMisc(entity: LivingEntity) {
         entity.addCustomAttribute(CustomAttribute(CustomAttributeTypes.MORE_DAMAGE_TAKEN_WHILE_ON_FIRE, roll = DoubleRoll(DoubleBounds(0.1))))
