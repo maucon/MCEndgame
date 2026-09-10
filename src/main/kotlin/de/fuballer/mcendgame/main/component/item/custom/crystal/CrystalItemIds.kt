38a32067117801db230fa6e1b4b2d58e44d1327d
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-09-10 15:11:42 +0200

    add synthesized crystal used statistics fix item ids fix endurance crystal texture name

--- src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/crystal/CrystalItemIds.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/item/custom/crystal/CrystalItemIds.kt
@@ -13,10 +13,10 @@ object CrystalItemIds {
     val IMITATION_CRYSTAL = RegistryKeyUtil.createItemKey("imitation_crystal")
 
     val SYNTHESIZED_FORCE_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_force_crystal")
-    val SYNTHESIZED_MOMENTUM_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_momentum")
-    val SYNTHESIZED_ENDURANCE_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_endurance")
-    val SYNTHESIZED_VITALITY_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_vitality")
-    val SYNTHESIZED_PRECISION_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_precision")
-    val SYNTHESIZED_FOCUS_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_focus")
-    val SYNTHESIZED_COMMAND_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_command")
+    val SYNTHESIZED_MOMENTUM_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_momentum_crystal")
+    val SYNTHESIZED_ENDURANCE_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_endurance_crystal")
+    val SYNTHESIZED_VITALITY_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_vitality_crystal")
+    val SYNTHESIZED_PRECISION_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_precision_crystal")
+    val SYNTHESIZED_FOCUS_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_focus_crystal")
+    val SYNTHESIZED_COMMAND_CRYSTAL = RegistryKeyUtil.createItemKey("synthesized_command_crystal")
 }
