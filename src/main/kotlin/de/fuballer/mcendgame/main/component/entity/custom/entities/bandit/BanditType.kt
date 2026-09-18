b3041166d434f1e05e04a28437fda3dbd036659e
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-09-18 18:45:34 +0200

    brina tankier

--- src/main/kotlin/de/fuballer/mcendgame/main/component/entity/custom/entities/bandit/BanditType.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/entity/custom/entities/bandit/BanditType.kt
@@ -187,8 +187,8 @@ enum class BanditType(
                     ),
                 ),
                 CustomAttribute(CustomAttributeTypes.RESISTANCE_WHEN_LOW_HEALTH, roll = IntRoll(IntBounds(10))),
-                CustomAttribute(CustomAttributeTypes.ARMOR_WHILE_WITHERED, roll = DoubleRoll(DoubleBounds(5.0))),
-                CustomAttribute(CustomAttributeTypes.MORE_DAMAGE_TAKEN, roll = DoubleRoll(DoubleBounds(-0.15))),
+                CustomAttribute(CustomAttributeTypes.ARMOR_WHILE_WITHERED, roll = DoubleRoll(DoubleBounds(4.0))),
+                CustomAttribute(CustomAttributeTypes.MORE_DAMAGE_TAKEN, roll = DoubleRoll(DoubleBounds(-0.3))),
             )
         },
     ),
