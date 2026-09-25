f520d5b930a119c7fe9a290ac94bf9d7fc40dfa0
Moewe342 <58944806+Moewe342@users.noreply.github.com>
2026-09-25 17:57:24 +0200

    add beastweaver blessing status effect icons

--- src/main/kotlin/de/fuballer/mcendgame/main/component/status_effect/HowlOfTheWolfEffect.kt
+++ src/main/kotlin/de/fuballer/mcendgame/main/component/status_effect/HowlOfTheWolfEffect.kt
@@ -4,11 +4,12 @@ import de.fuballer.mcendgame.main.component.custom_attribute.data.CustomAttribut
 import de.fuballer.mcendgame.main.component.custom_attribute.data.DoubleBounds
 import de.fuballer.mcendgame.main.component.custom_attribute.data.DoubleRoll
 import de.fuballer.mcendgame.main.component.custom_attribute.types.CustomAttributeTypes
+import de.fuballer.mcendgame.main.util.ColorUtil
 import de.fuballer.mcendgame.main.util.minecraft.IdentifierUtil
 import net.minecraft.world.effect.MobEffect
 import net.minecraft.world.effect.MobEffectCategory
 
-class HowlOfTheWolfEffect : MobEffect(MobEffectCategory.BENEFICIAL, 9835540) {
+class HowlOfTheWolfEffect : MobEffect(MobEffectCategory.BENEFICIAL, ColorUtil.rgbaToInt(95, 141, 72, 255)) {
     companion object {
         val ATTRIBUTE_IDENTIFIER = IdentifierUtil.default("effect.howl_of_the_wolf")
 
